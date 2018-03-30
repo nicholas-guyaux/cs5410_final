47882169321166b9a053f99edb1de00566bdf8dd
Sam Christiansen <boonezar@yahoo.com>
2018-03-30 14:16:02 -0600

    Moving sound loading into loader

--- client_files/loader.js
+++ client_files/loader.js
@@ -172,6 +172,9 @@ MyGame.loader = (function() {
     }
 
     function mainComplete(){
+      AudioPool.addMusic('menu', 'assets/sound/09 Come and Find Me - B mix.mp3');
+      AudioPool.addSFX('menu_click', 'assets/sound/270324__littlerobotsoundfactory__menu-navigate-00.wav');
+      AudioPool.addSFX('menu_navigate', 'assets/sound/270322__littlerobotsoundfactory__menu-navigate-02.wav');
       console.log('Loading Complete');
       //MainView.loadView(MenuView.name);
       window.main('MenuView');
