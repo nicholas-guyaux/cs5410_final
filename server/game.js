c122b6b4387cd4c41d49da26418c6f52fd77b4ac
Nicholas Guyaux <nicholas.guyaux@gmail.com>
2018-04-17 13:03:20 -0600

    added kills

--- server/game.js
+++ server/game.js
@@ -139,7 +139,6 @@ function checkPlayerVsBulletCollisions(player, clientId){
         player.health.current -= results[i].damage;
         if (player.health.current <= 0) {
           GameState.gameClients[results[i].clientId].state.player.killCount++;
-          console.log('kills:',GameState.gameClients[results[i].clientId].state.player.killCount);
         }
         player.reportUpdate = true;
         bulletTree.remove(results[i]);
