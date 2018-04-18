26e50a01f91308e7edd8fc8f83e9a8357d1a310a
John D. Johnson II <johnsonjo4531@gmail.com>
2018-04-17 22:06:14 -0600

    fixed bug deadly bug

--- server/game.js
+++ server/game.js
@@ -253,7 +253,7 @@ function update(elapsedTime, currentTime, totalTime) {
   bulletTree.load(activeBullets);
   for (let clientId in GameState.gameClients) {
     checkCollisions(GameState.gameClients[clientId].state.player, GameState.gameClients[clientId].socket.id);
-    if(checkDeath(GameState.gameClients[clientId].state.player))
+    if(checkDeath(GameState.gameClients[clientId].state.player)) {
       processDeath(GameState.gameClients[clientId].state.player);
       GameState.gameClients[clientId].socket.emit(GameNetIds.MESSAGE_GAME_OVER, {
         // they are not subtracted yet from the alive players so this is their position.
